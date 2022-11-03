948a31244113d2c3bfad7f8ec4a6128199fc1f06
DizzlyD <david4henery00@hotmail.com>
2022-11-03 10:43:09 +0000

    ui-refactor

--- src/components/ColumnHeading.js
+++ src/components/ColumnHeading.js
@@ -1,13 +1,23 @@
 import React from "react";
 
 const ColumnHeading = ({ name, quantity }) => {
+  //
   return (
     <div className="column-heading-section flex-cen-cen">
-      <div className="column-heading-section__icon"></div>
+      <div
+        className="column-heading-section__icon"
+        style={{
+          backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(
+            16
+          )}`,
+        }}
+      ></div>
       <h2 className="heading-s column-heading-section__title">
         {name && name.toUpperCase()}
       </h2>
-      <p className="heading-s column-heading-section__tasks-quantity">({quantity})</p>
+      <p className="heading-s column-heading-section__tasks-quantity">
+        ({quantity})
+      </p>
     </div>
   );
 };
